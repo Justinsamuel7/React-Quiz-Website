e48257097e8531d412fc7607bf90d648539415ed
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-04-01 15:45:51 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useNavigate} from "react";
 import "./style.css";
 import {Router,Route,Link} from 'react-router-dom'
 
@@ -13,33 +13,33 @@ function storename()
 export default function App() {
   let [Timer,setTimer] = useState(false)
 
-  setTimeout(()=>{
-    setTimer(true)
-  },5000)
+  // setTimeout(()=>{
+  //   setTimer(true)
+  // },5000)
 
-  function Questions()
-  {
-    return(
-      <>
-      <h1>question1</h1>
-      </>
-    )
-  }
+  // function Questions()
+  // {
+  //   return(
+  //     <>
+  //     <h1>question1</h1>
+  //     </>
+  //   )
+  // }
 
-  function Result()
-  {
-    return(
-      <>
-     <h1>result</h1>
-      </>
-    )
-  }
+  // function Result()
+  // {
+  //   return(
+  //     <>
+  //    <h1>result</h1>
+  //     </>
+  //   )
+  // }
 
   return (
     <div>
       <h1>Welcome to Test</h1>
       <p>Please enter your name</p><input type="text" placeholder="Name" id="nameinpbox"/><button onClick={storename}>Enter</button>
-      {Timer? Result() : Questions()}
+      {/* {Timer? Result() : Questions()} */}
     </div>
   );
 }
