d13a1ceb83cd2eec65ea322c84f84e2cd791c518
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-04-04 14:48:42 +0530

    1 file modified, 2 added, 2 deleted

--- src/TestInterface.js
+++ src/TestInterface.js
@@ -1,7 +1,9 @@
 import React,{useState,useNavigate} from "react";
 import "./style.css";
-import {Router,Route,Link} from 'react-router-dom'
-import Q1 from './Questions/Q1';
+import {Routes,Route,Link,useNavigate} from 'react-router-dom'
+import Q1 from "./Q1";
+import Q2 from "./Q2";
+
 
 
 export default function TestInterface() {
@@ -14,34 +16,42 @@ export default function TestInterface() {
 
           <div id="buttonsContainer">
             <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>10</button>
-            <button>11</button>
-            <button>12</button>
-            <button>13</button>
-            <button>14</button>
-            <button>15</button>
-            <button>16</button>
-            <button>17</button>
-            <button>18</button>
-            <button>19</button>
-            <button>20</button>
+            <button disabled="true">2</button>
+            <button disabled="true">3</button>
+            <button disabled="true">4</button>
+            <button disabled="true">5</button>
+            <button disabled="true">6</button>
+            <button disabled="true">7</button>
+            <button disabled="true">8</button>
+            <button disabled="true">9</button>
+            <button disabled="true">10</button>
+            <button disabled="true">11</button>
+            <button disabled="true">12</button>
+            <button disabled="true">13</button>
+            <button disabled="true">14</button>
+            <button disabled="true">15</button>
+            <button disabled="true">16</button>
+            <button disabled="true">17</button>
+            <button disabled="true">18</button>
+            <button disabled="true">19</button>
+            <button disabled="true">20</button>
 
           </div>
 
         </div>
 
         <div id="Qpart">
-          <Q1/>
+          <Routes >
+            <Route path="/" element={<Q1/>}/>
+            <Route path="/Q2" element={<Q2/>}/>
+          </Routes>
+
+          <button onClick={}>Prev</button>
+          <button onClick={}>Next</button>
 
         </div>
+
+
 
       </div>
       
