564b6842522b18173039450ecc812ee082e2ab4b
Justinsamuel7 <119278858+Justinsamuel7@users.noreply.github.com>
2023-04-09 16:12:37 +0530

    1 file modified

--- src/Questions.js
+++ src/Questions.js
@@ -55,54 +55,54 @@ let QuestionsArr=[
     Answer:"PSG"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Who have won the premier league most?",
+    options:["Man Utd","Man City","Chelsea","Liverpool"],
+    Answer:"Man Utd"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Who have won the La Liga most?",
+    options:["Barcelona","Atletico de Madrid","Sevilla","Real Madrid"],
+    Answer:"Real Madrid"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"ballon D'or Winner in 2022?",
+    options:["Benzema","Modric","Salah","Vinicius Jr"],
+    Answer:"Benzema"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"International Top Goal Scorer is?",
+    options:["Neymar","Ronaldo","Chettri","Messi"],
+    Answer:"Ronaldo"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Which of the following player not played for Chelsea?",
+    options:["A Morata","T Abraham","T Werner","None of the above"],
+    Answer:"None of the above"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Brazil Eliminated from _______ in 2014 WC",
+    options:["Semi-finals","Quater-finals","RO16","Group-stage"],
+    Answer:"Semi-finals"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"UCL all time top goalscorer?",
+    options:["Aguero","Ronaldo","Messi","Lewandowski"],
+    Answer:"Ronaldo"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Who of the below is not a Midfielder?",
+    options:["Enzo fernandez","thiago alcantara","Kovacic","Araujo"],
+    Answer:"Araujo"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Zlatan Ibrahimovic not played for ________",
+    options:["LA Galaxy","Chelsea","Ajax","Man Utd"],
+    Answer:"Chelsea"
   },
   {
-    question:"which of the following player not played for real madrid?",
-    options:["Kubo","Robben","Kovacic","Falcao"],
-    Answer:"Falcao"
+    question:"Portugal Euro winning Year?",
+    options:["2012","2020","2016","Never Won"],
+    Answer:"2016"
   },
   
 ]
